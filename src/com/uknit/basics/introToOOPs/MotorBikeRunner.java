3795a271638f72f3e80f0d2bc8746d604dba3f50
yeswanth37 <yeswanth.mangalagiri@gmail.com>
2023-11-29 00:13:03 +0530

    commit

--- src/com/uknit/basics/introToOOPs/MotorBikeRunner.java
+++ src/com/uknit/basics/introToOOPs/MotorBikeRunner.java
@@ -2,4 +2,13 @@ package com.uknit.basics.introToOOPs;
 
 public class MotorBikeRunner {
 
+	public static void main(String[] args) {
+
+		MotorBike honda = new MotorBike();
+		MotorBike ducati = new MotorBike();
+
+		honda.start();
+		ducati.start();
+	}
+
 }
